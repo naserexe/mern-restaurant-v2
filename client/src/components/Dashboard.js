8c89d7c3a1e946ccab02116ba57604586f750b60
Naser <nasersoftcorporation2013@gmail.com>
2020-04-11 16:44:37 +0600

    Balance method and information added

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,22 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect, useContext } from 'react'
 import Dish from './dish/Dish';
-import Ingredient from './ingredient/Ingredient'
+import Ingredient from './ingredient/Ingredient';
+import Spinner from './layout/Spinner'
+
+import DishContext from '../context/dish/dishContext'
 
 const Dashboard = () => {
+  const dishContext  = useContext(DishContext);
+  const { balance, loading, getBalance } = dishContext
+  useEffect(() => {
+    getBalance();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <Fragment>
-      <h1 className='text-success'>Dashboard</h1>
+      <h1 className='text-info'>Dashboard</h1>
+      {balance && !loading ? (<h3 className='text-warning'>Balance: <strong className='text-success'>${balance.balanceAmount}</strong></h3>): <Spinner/>}
       <div className="row">
       <div className="col">
         <Ingredient/>
